78a279deda670fe06330a45b3e47944c7e71f8dd
Jakalgit <mistetsmit05@gmail.com>
2024-04-02 22:33:20 +0300

    change config.py

--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
 BOT_TOKEN = '6803589201:AAHIT78MsCH-ya12I2v0uJoxDds-qNp6Ah8'
 CHAT_ID = 588522164
-PATH_TO_FILE = "assets/test.pdf"
-PATH_TO_IMAGE = "assets/photo.jpg"
+PATH_TO_FILE = "./assets/test.pdf"
+PATH_TO_IMAGE = "./assets/photo.jpg"
